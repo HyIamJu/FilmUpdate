c6060957fdf13a0328eb0692e2f10c346c3f8ca6
HyIamJu <junandaikarizky10@gmail.com>
2023-05-15 22:00:25 +0700

    few change

---
+++ film_update_mobile_apps/build/flutter_inappwebview/generated/source/buildConfig/release/com/pichillilorenzo/flutter_inappwebview/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pichillilorenzo.flutter_inappwebview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.pichillilorenzo.flutter_inappwebview";
+  public static final String BUILD_TYPE = "release";
+}
